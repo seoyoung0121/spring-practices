4f526d039b33506f6108a6471fb34d5b52311281
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-12-31 10:48:36 +0900

    guestbook03 - feat: datasource 사용

--- guestbook03/src/main/java/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package guestbook.controller;
 
+
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import guestbook.repository.GuestbookRepository;
 import guestbook.vo.GuestbookVo;
 
+
 @Controller
 public class GuestbookController {
 	private GuestbookRepository guestbookRepository;
@@ -21,7 +23,25 @@ public class GuestbookController {
 	}
 	
 	@RequestMapping("/")
-	public String index(Model model) {
+	public String index(/*HttpServletRequest request,*/ Model model) {
+	
+		/* ServletContext sc = request.getServletContext();
+		Enumeration<String> e = sc.getAttributeNames();
+		while(e.hasMoreElements()) {
+			String name = e.nextElement();
+			System.out.println(name);
+		}
+		ApplicationContext ac1 = (ApplicationContext) sc.getAttribute("org.springframework.web.context.WebApplicationContext.ROOT");
+		ApplicationContext ac2 = (ApplicationContext) sc.getAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.spring");
+		
+		GuestbookRepository repository = ac1.getBean(GuestbookRepository.class);
+		System.out.println(repository);
+		
+		GuestbookController controller = ac2.getBean(GuestbookController.class);
+		System.out.println(controller);
+		
+		System.out.println(ac1==ac2);
+		*/
 		List<GuestbookVo> list = guestbookRepository.findAll();
 		model.addAttribute("list", list);
 		return "index";
--- guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
+++ guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
@@ -1,12 +1,13 @@
 package guestbook.repository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.springframework.stereotype.Repository;
 
@@ -14,11 +15,14 @@ import guestbook.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
-
+	private DataSource dataSource;
+	public GuestbookRepository(DataSource dataSource) {
+		this.dataSource=dataSource;
+	}
 	public List<GuestbookVo> findAll() {
 		List<GuestbookVo> result = new ArrayList<>();
 		
-		try (Connection conn = getConnection();
+		try (Connection conn = dataSource.getConnection();
 			PreparedStatement pstmt = conn.prepareStatement("select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc");
 			ResultSet rs = pstmt.executeQuery();
 			){
@@ -46,7 +50,7 @@ public class GuestbookRepository {
 		int count = 0;
 
 
-		try (Connection conn = getConnection();
+		try (Connection conn = dataSource.getConnection();
 			PreparedStatement pstmt = conn.prepareStatement("insert into guestbook values(null, ?, ?, ?, now())");
 			){
 
@@ -66,7 +70,7 @@ public class GuestbookRepository {
 	public int deleteByIdAndPassword(Long id, String password) {
 		int count = 0;
 		
-		try (Connection conn = getConnection();
+		try (Connection conn = dataSource.getConnection();
 			PreparedStatement pstmt = conn.prepareStatement("delete from guestbook where id = ? and password = ?");
 			){
 
@@ -80,22 +84,6 @@ public class GuestbookRepository {
 		}
 		return count;
 		
-	}
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-
-			String url = "jdbc:mariadb://192.168.0.18:3306/webdb";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		return conn;
 	}
 	
 }
