75c0c82ea89f9a24278fc3b52eda867eb1f99cfa
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-02 15:44:18 +0900

    file upload - feat: multipart 기초

---
+++ fileupload/src/main/java/fileupload/controller/FileUploadController.java
@@ -0,0 +1,13 @@
+package fileupload.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FileUploadController {
+	
+	@RequestMapping({"/", "/form"})
+	public String form() {
+		return "form";
+	}
+}
