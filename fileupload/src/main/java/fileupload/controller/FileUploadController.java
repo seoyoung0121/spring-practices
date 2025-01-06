964a33857be7c1b91ece4a98524138ad041d15da
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-06 13:15:09 +0900

    mysite03 - feat: 한영 변환 기능 추가

--- fileupload/src/main/java/fileupload/controller/FileUploadController.java
+++ fileupload/src/main/java/fileupload/controller/FileUploadController.java
@@ -1,6 +1,5 @@
 package fileupload.controller;
 
-import java.io.IOException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- locale/src/main/java/locale/controller/LocaleController.java
+++ locale/src/main/java/locale/controller/LocaleController.java
@@ -1,6 +1,7 @@
 package locale.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.LocaleResolver;
 
@@ -15,8 +16,11 @@ public class LocaleController {
 	}
 	
 	@RequestMapping("/")
-	public String index(HttpServletRequest request) { // 이 처리는 아마 interceptor같은데서 해야
-		System.out.println("resolver-locale: " + localeResolver.resolveLocale(request).getLanguage());
+	public String index(HttpServletRequest request, Model model) { // 이 처리는 아마 interceptor같은데서 해야
+		String lang = localeResolver.resolveLocale(request).getLanguage();
+		System.out.println("Language Code: " + lang);
+		
+		model.addAttribute("lang", lang);
 		return "index";
 	}
 	
